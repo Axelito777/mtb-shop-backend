66c5bbda62e1592ae5e35b962e59f730fed46b18
Axelito777 <ax.subiabre@duocuc.cl>
2025-12-16 14:46:37 -0300

    Fix: Add DatabaseConfig to parse Railway DATABASE_URL correctly

---
+++ src/main/java/com/mtbshop/config/DatabaseConfig.java
@@ -0,0 +1,35 @@
+package com.mtbshop.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Configuration
+@Profile("prod")
+public class DatabaseConfig {
+
+    @Value("${DATABASE_URL}")
+    private String databaseUrl;
+
+    @Bean
+    public DataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(databaseUrl);
+        
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+        return DataSourceBuilder.create()
+                .url(dbUrl)
+                .username(username)
+                .password(password)
+                .driverClassName("org.postgresql.Driver")
+                .build();
+    }
+}
